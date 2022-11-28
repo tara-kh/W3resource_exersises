f62077863c5f15c836128f171d1eee7819f6a439
tara-kh <khorramnia.tara@gmail.com>
2022-11-28 12:51:20 +0100

    problem solved

---
+++ Conditional Statement/10.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+
+    int Math, Phy, Chem;
+    int sum1, sum2;
+
+    printf("Input the marks obtained in Mathematics: ");
+    scanf("%d", &Math);
+
+    printf("Input the marks obtained in Physics: ");
+    scanf("%d", &Phy);
+
+    printf("Input the marks obtained in Chemistry; ");
+    scanf("%d", &Chem);
+
+    sum1 = Math + Phy + Chem;
+    sum2 = Math + Phy;
+
+    if ((Math >= 65 && Phy >= 55 && Chem >= 50) && (sum1 >= 190 || sum2 >= 140))
+    {
+        printf("The candidate is eligible for admission.\n");
+    }
+
+    else
+    {
+        printf("The candidate is not eligible for admission.\n");
+    }
+
+    return 0;
+}
