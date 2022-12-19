1f05b682cf5fb7901b0d3d91ae44fc0ae78f98e3
tara-kh <khorramnia.tara@gmail.com>
2022-12-19 21:55:11 +0100

    challenge solved

--- pointer/1.c
+++ pointer/1.c
@@ -5,13 +5,25 @@ int main()
     printf("Pointer : Show the basic declaration of pointer : \n");
     int m = 10;
     int n, o;
-    int *z = &m;
+    int *z;
 
+    z = &m;
+    printf(" z stores the address of m  = 0x%x\n", z);
     printf("z stores the address of m  = %p\n", z);
-    printf(" *z stores the value of m = %i \n", *z);
+
+    printf(" *z stores the value of m = 10\n", *z);
+
+    printf(" &m is the address of m = 0x%x\n", &m);
     printf(" &m is the address of m = %p\n", &m);
-    printf(" &n stores the address of n = %p\n", &n);
-    printf(" &o  stores the address of o = %p\n", &o);
-    printf(" &z stores the address of z = %p\n", &z);
+
+    printf(" &n is the address of n = 0x%x\n", &n);
+    printf(" &n is the address of n = %p\n", &n);
+
+    printf(" &o is the address of o = 0x%x\n", &o);
+    printf(" &o is the address of o = %p\n", &o);
+
+    printf(" &z is the address of z = 0x%x\n", &z);
+    printf(" &z is the address of z = %p\n", &z);
+
     system("PAUSE");
 }
